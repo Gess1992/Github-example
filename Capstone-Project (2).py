d9307b2f49f11dc58586199d5f331818a1925042
Chrisnelle-McCanna <57201986+Gess1992@users.noreply.github.com>
2020-03-24 15:30:45 -0400

    add sample notebook

---
+++ Capstone-Project (2).py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  # Capstone-Project
+#  
+#  ### Jupyter Notebook use any platform that you are comfortable with
+#  
+
+# In[1]:
+
+
+import pandas as pd
+from bokeh.plotting import figure, output_file, show,output_notebook
+output_notebook()
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
